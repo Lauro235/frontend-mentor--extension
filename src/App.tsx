7982132f5b38b8afa9be5670ab6ace1ee93a6f8f
Lorentz <91217473+Lauro235@users.noreply.github.com>
2023-02-20 14:17:50 +0000

    successfully map cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './Styles/base/reset.scss';
 import './_App.scss';
-import useWindowSize from './Hooks/useWindowSize';
-import Card from './Components/Card/Card';
+// import useWindowSize from './Hooks/useWindowSize';
+import Cards from './Components/Cards/Cards';
 
 function App() {
   // const { height } = useWindowSize();
 
   return (
     <div className="App">
-      <Card />
-      <Card /* height={height} */ />
+      <Cards />
     </div>
   );
 }
